c8b8de35e0c04475b728c0ce61e9c5f1b8769e39
Juhui Kim <kjh107704@gmail.com>
2023-02-12 18:50:24 +0900

    [Add] 회원 서비스 클래스 구현

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    // 가입을 하고 사람을 찾고 하려면 MemberRepository interface가 필요함
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
