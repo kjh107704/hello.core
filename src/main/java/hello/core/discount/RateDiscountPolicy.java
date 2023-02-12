8763b55242434c9c573a3500fb4680efe3281ae6
Juhui Kim <kjh107704@gmail.com>
2023-02-12 20:23:15 +0900

    [Test] 정률 할인 정책 테스트코드 추가

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -6,6 +6,8 @@ import hello.core.member.Member;
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
+
+    // cmd+shift+t 누르면 해당 메소드의 테스트코드로 바로 가기/생성 할 수 있음
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP) {
