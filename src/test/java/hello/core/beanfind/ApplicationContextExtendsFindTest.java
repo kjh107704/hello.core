f91a5ba6945a513d06d0da4110d9356e1dfe7036
Juhui Kim <kjh107704@gmail.com>
2023-02-19 19:47:20 +0900

    feat: 상속 관계 Bean 조회 Test

---
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,73 @@
+package hello.core.beanfind;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationContextExtendsFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면 오류가 발생한다")
+    void findBeanByParentTypeDuplicate(){
+
+        assertThrows(NoUniqueBeanDefinitionException.class,
+            () -> ac.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면 이름으로 조회하면 된다")
+    void findBeanByName() {
+        DiscountPolicy rateDiscounPolicy = ac.getBean("rateDiscount Policy", DiscountPolicy.class);
+
+        Assertions.assertThat(rateDiscounPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회하기")
+    void findAllBeansByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+
+        for (String s : beansOfType.keySet()) {
+            System.out.println("s = " + s + " value = " + beansOfType.get(s));
+        }
+
+        Assertions.assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 조회하기 - Object class")
+    void findAllBeansByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+
+        for (String s : beansOfType.keySet()) {
+            System.out.println("s = " + s + " value = " + beansOfType.get(s));
+        }
+    }
+
+
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new FixDiscountPolicy();
+        }
+    }
+}
