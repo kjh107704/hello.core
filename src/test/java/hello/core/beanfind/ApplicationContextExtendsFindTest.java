18f6c547eaf38cfee79ad419c287d85a3bca9dbb
Juhui Kim <kjh107704@gmail.com>
2023-03-11 23:24:16 +0900

    fix: Bean 이름 수정

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -29,7 +29,7 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면 이름으로 조회하면 된다")
     void findBeanByName() {
-        DiscountPolicy rateDiscounPolicy = ac.getBean("rateDiscount Policy", DiscountPolicy.class);
+        DiscountPolicy rateDiscounPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
 
         Assertions.assertThat(rateDiscounPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
