e7f13d4915ac0c21aee4403545542a18ab8bcc0a
Juhui Kim <kjh107704@gmail.com>
2023-03-12 23:16:25 +0900

    feat: 의존관계 주입 시, 같은 타입의 bean이 2개 이상일 때 @Primary로 해결

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -19,7 +19,7 @@ public class OrderServiceImpl implements OrderService{
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
     // DIP를 지키기 위해서는 구체 클래스를 걷어내면 되는데, 이러면 실행이 안됨 => 실행할 때 객체가 할당되지 않으니까
-    private final @Qualifier("mainDiscountPolicy")DiscountPolicy rateDiscountPolicy;
+    private final DiscountPolicy rateDiscountPolicy;
 
     /*
      * lombok의 @RequiredArgsConstructor 로 대체략
