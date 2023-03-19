ef9962cd43f5bf02887ce4970140ce4ecd081b9b
Juhui Kim <kjh107704@gmail.com>
2023-03-19 22:37:01 +0900

    feat: Spring InitializingBean, DisposableBean을 적용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,27 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
-    public NetworkClient() {
-        System.out.println("생성자 호출, url = " + url);
-
+    // InitializingBean이 지원하는 함수로, 의존관계 주입이 끝나면 해당 method를 call 함
+    @Override
+    public void afterPropertiesSet() throws Exception {
         connect();
         call("초기화 연결 메세지");
+    }
+
+    // bean이 없어지기 전에 해당 method를 call 함
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
     }
 
     public void setUrl(String url) {
